95de384086ac6cede8e2d76af4b12bcabc214da7
aden <aden.pribadi@gmail.com>
2026-04-28 13:58:29 +0700

    port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       max_memory_restart: '512M',
       env: {
         NODE_ENV: 'production',
-        PORT: 3000
+        PORT: 3001
       },
       error_file: './logs/pm2-error.log',
       out_file: './logs/pm2-out.log',
